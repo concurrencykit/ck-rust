02726629f29f17ac587676dbd76a726c49bd1f63
Samy Bahra <sbahra@gothamtrails.com>
2024-09-13 10:01:24 -0400

    Initial import of empty crate.

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+//! Modern concurrency primitives and building blocks for high performance applications.
+//!
+//! This is a placeholder for a library in progress.
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
